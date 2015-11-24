b5de0fd46220f99563b55998666ebefecaa4d4c0
Skyler Dickey <Skyler.Dickey@Maryville.com>
2015-11-24 09:05:34 -0600

    Fixed path issue

--- routers/router.go
+++ routers/router.go
@@ -1,19 +1,21 @@
 package routers
 
 import (
+	"enteonsummit/controllers"
+	"enteonsummit/controllers/rest"
+
 	"github.com/astaxie/beego"
-	"edu/Task_07_Angular/task07angular/controllers"
-	"edu/Task_07_Angular/task07angular/controllers/rest"
 )
 
 func init() {
-  beego.Router("/rest/echo/:val", &rest.EchoController{})
+	beego.Router("/rest/echo/:val", &rest.EchoController{})
 	beego.Router("/rest/echo4/:val", &rest.EchoController4{})
-  beego.Router("/rest/echo", &rest.EchoController{})
-  beego.Router("/rest/echo3", &rest.EchoController3{})
+	beego.Router("/rest/echo", &rest.EchoController{})
+	beego.Router("/rest/echo3", &rest.EchoController3{})
 	beego.Router("/rest/echo4", &rest.EchoController4{})
 	beego.Router("/page4", &controllers.Page4Controller{})
-  beego.Router("/page3", &controllers.Page3Controller{})
-  beego.Router("/page2", &controllers.Page2Controller{})
+	beego.Router("/page3", &controllers.Page3Controller{})
+	beego.Router("/page2", &controllers.Page2Controller{})
+
 	beego.Router("/", &controllers.IndexController{})
 }
