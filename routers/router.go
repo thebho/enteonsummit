17a2f1441edae2f971f4a9352f5f5f3a6cde7342
Brian Hoehne <brianhoehne@gmail.com>
2015-11-24 10:44:47 -0600

    container work

--- routers/router.go
+++ routers/router.go
@@ -12,7 +12,7 @@ func init() {
 	beego.Router("/rest/echo/:val", &rest.EchoController{})
 	beego.Router("/rest/echo", &rest.EchoController{})
 	beego.Router("/rest/echo3", &rest.EchoController3{})
-	beego.Router("/page3", &controllers.Page3Controller{})
+	beego.Router("/containers", &controllers.Page3Controller{})
 	beego.Router("/images", &controllers.Page2Controller{})
 
 	beego.Router("/", &controllers.IndexController{})
