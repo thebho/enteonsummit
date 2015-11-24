859858d8a184fb391f75749b2642c85b4150fee6
Brian Hoehne <brianhoehne@gmail.com>
2015-11-24 09:44:24 -0600

    no page 4

--- routers/router.go
+++ routers/router.go
@@ -10,11 +10,9 @@ import (
 func init() {
 	beego.Router("/rest/docker/image/:command/:value", &rest.DockerImageRest{})
 	beego.Router("/rest/echo/:val", &rest.EchoController{})
-	beego.Router("/rest/echo4/:val", &rest.EchoController4{})
 	beego.Router("/rest/echo", &rest.EchoController{})
 	beego.Router("/rest/echo3", &rest.EchoController3{})
 	beego.Router("/rest/echo4", &rest.EchoController4{})
-	beego.Router("/page4", &controllers.Page4Controller{})
 	beego.Router("/page3", &controllers.Page3Controller{})
 	beego.Router("/page2", &controllers.Page2Controller{})
 
